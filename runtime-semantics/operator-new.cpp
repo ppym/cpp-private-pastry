1acd0519c23b21e77066a3facb0244de87ff7c0f
ppym <pengyimin1988@gmail.com>
2018-02-09 12:33:49 +0800

    add stack test

--- runtime-semantics/operator-new.cpp
+++ runtime-semantics/operator-new.cpp
@@ -9,6 +9,7 @@ public:
 	int getMi() {return mi;}
 	void * operator new(size_t size);
 	void * operator new(size_t size, const std::string &str);
+	void * operator new(size_t size, void *buf);
 	void operator delete(void *);
 private:
 	int mi;
@@ -22,7 +23,15 @@ void *X::operator new(size_t size)
 
 void *X::operator new(size_t size, const std::string &str)
 {
-	cout << "X::operator new(size_t): " << size << " " << str << endl;
+	cout << "X::operator new(size_t, const std::string&): " << size << " " << str << endl;
+	return ::operator new(size); //call origin operator new
+}
+
+void *X::operator new(size_t size, void *buf)
+{
+	cout << "X::operator new(size_t, void*): " << size << " " << buf << endl;
+	if (buf)
+		return buf;
 	return ::operator new(size); //call origin operator new
 }
 
@@ -63,11 +72,16 @@ int main()
 	//3.return the pointer
 	px2->getMi();
 
+	char buf[sizeof(X)];
+	X* px3 = new (buf) X();
+	cout << "px3 addr=" << px3 << endl;
+
 	//within the delete operator below(that is c++-built-in), compiler will do 2 steps
 	//1.call the X destructor 
 	//2.call the operator delete
 	delete px1;
 	delete px2;
+	delete px3; //crash since we use stack memory
 
 	int *pi = new int(5);
 	delete pi;
