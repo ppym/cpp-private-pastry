774cf84ce9c51c85ebe18ae31064226083885a27
ppym <pengyimin1988@gmail.com>
2018-02-25 12:24:49 +0800

    三种策略

--- algorithm/knapsack1.cpp
+++ algorithm/knapsack1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 
@@ -25,6 +26,8 @@ public:
 		totalP += o.price;
 		return true;
 	}
+	void reset() {objV.clear(); currC =0; totalP = 0;}
+
 	unsigned int getCurrC() { return currC;}
 	unsigned int getTotalC() { return totalC;}
 	unsigned int getTotalPrice() { return totalP;}
@@ -60,24 +63,72 @@ void PrintSelectedObject(C c)
 	std::cout << std::endl;
 }
 
-int main()
+void selectByWeight(Bag &bag, std::vector<Object> &objs)
 {
-	std::vector<Object> objCur = { {35,10},{30,40}, {60,30},{50,50},{40,35},{10,40},{25,30}};
-	Bag bag(150);
-
 	//按照重量最轻选择
-	sort (objCur.begin(), objCur.end(), [] (Object o1, Object o2) { return o1.weigth < o2.weigth; });
-	PrintContaiiner(objCur);
+	sort (objs.begin(), objs.end(), [] (Object o1, Object o2) { return o1.weigth < o2.weigth; });
 
-	for (Object &o : objCur) {
-		if (o.status == UnSelect && (o.weigth + bag.getCurrC()) < bag.getTotalC()) {
+	for (Object &o : objs) {
+		if (o.status == UnSelect && (o.weigth + bag.getCurrC()) <= bag.getTotalC()) {
 			bag.addObject(o);
 			o.status = Selected;
 		}
 	}
-	PrintContaiiner(objCur);
-	std::cout << "Bag total Contain: " << bag.getTotalC() << endl;
-	std::cout << "Bag Curr Contain: " << bag.getCurrC() << endl;
-	std::cout << "Bag total Price: " << bag.getTotalPrice() << endl;
+}
+	
+void selectByPrice(Bag &bag, std::vector<Object> &objs)
+{
+	sort (objs.begin(), objs.end(), [] (Object o1, Object o2) { return o1.price >= o2.price; });
+
+	for (Object &o : objs) {
+		if (o.status == UnSelect && (o.weigth + bag.getCurrC()) <= bag.getTotalC()) {
+			bag.addObject(o);
+			o.status = Selected;
+		}
+	}
+}
+
+void selectByDensity(Bag &bag, std::vector<Object> &objs)
+{
+	sort (objs.begin(), objs.end(), [] (Object o1, Object o2) { return (double)o1.price/o1.weigth >= (double)o2.price/o2.weigth; });
+
+	for (Object &o : objs) {
+		if (o.status == UnSelect && (o.weigth + bag.getCurrC()) <= bag.getTotalC()) {
+			bag.addObject(o);
+			o.status = Selected;
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	std::vector<Object> objCur = { {35,10},{30,40}, {60,30},{50,50},{40,35},{10,40},{25,30}};
+	Bag bag(150);
+	{
+		std::vector<Object> objs(objCur);
+
+		selectByWeight(bag, objs);
+		PrintContaiiner(objs);
+		std::cout << "Bag selece Result[" << bag.getTotalC() << ',' << bag.getCurrC() << ',' << bag.getTotalPrice() << ']' << endl;
+	}
+
+	{
+		std::vector<Object> objs(objCur);
+		bag.reset();
+
+		selectByPrice(bag, objs);
+		PrintContaiiner(objs);
+		std::cout << "Bag selece Result[" << bag.getTotalC() << ',' << bag.getCurrC() << ',' << bag.getTotalPrice() << ']' << endl;
+	}
+
+	{
+		std::vector<Object> objs(objCur);
+		bag.reset();
+
+		selectByDensity(bag, objCur);
+		PrintContaiiner(objCur);
+		std::cout << "Bag selece Result[" << bag.getTotalC() << ',' << bag.getCurrC() << ',' << bag.getTotalPrice() << ']' << endl;
+	}
+	return 0;
 }
 
